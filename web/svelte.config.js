c7a68bdaacd21a2c517391edd74ef55d074cc3b8
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2023-02-10 16:02:01 +0100

    Push vercel web config

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,8 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 const config = {
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            runtime: 'edge',
+            regions: ['fra1'],
+        }),
     },
 };
 
