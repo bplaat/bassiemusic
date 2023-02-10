7e6d3cec0752dcb78afa7c440044254a6c91d43e
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2023-02-10 18:53:59 +0100

    Fixed infinite load bug

--- web/src/routes/settings/+page.server.js
+++ web/src/routes/settings/+page.server.js
@@ -25,6 +25,6 @@ export async function load({ cookies, fetch }) {
         authUser,
         currentSession,
         sessions,
-        sessionsToal: pagination.total,
+        sessionsTotal: pagination.total,
     };
 }
