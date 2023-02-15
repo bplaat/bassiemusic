50c41a8a74caf72c814b359e263ce078c579f0f5
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2023-02-15 08:02:44 +0100

    Cleaned up some code

--- web/src/components/album-card.svelte
+++ web/src/components/album-card.svelte
@@ -4,32 +4,24 @@
     export let album;
     export let token;
 
-    async function fetchTracks() {
+    async function fetchAndPlayTracks() {
         const response = await fetch(`${import.meta.env.VITE_API_URL}/albums/${album.id}`, {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
         });
-        let albumData = await response.json();
-        let tracks = albumData['tracks'];
-
-        albumData.tracks = albumData.tracks.slice().map((track) => {
+        const completeAlbum = await response.json();
+        const tracks = completeAlbum.tracks.map((track) => {
             track.album = album;
             return track;
         });
-
         $musicPlayer.playTracks(tracks, tracks[0]);
     }
 </script>
 
-<a class="card" href="/albums/{album.id}">
+<a class="card has-image-play-button" href="/albums/{album.id}">
     <div class="card-image has-image-tags" style="aspect-ratio: 1;">
         <img src={album.medium_cover} alt="Cover of album {album.name}" loading="lazy" />
-        <button class="button is-outlined play-button-image" on:click|preventDefault={() => fetchTracks()}>
-            <svg class="icon" viewBox="0 0 24 24">
-                <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
-            </svg>
-        </button>
         <div class="image-tags">
             {#if album.type == 'album'}
                 <span class="tag">ALBUM</span>
@@ -44,6 +36,11 @@
                 <span class="tag is-danger" title="Explicit lyrics">E</span>
             {/if}
         </div>
+        <button class="button image-play-button" on:click|preventDefault={fetchAndPlayTracks}>
+            <svg class="icon" viewBox="0 0 24 24">
+                <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+            </svg>
+        </button>
     </div>
     <div class="card-content">
         <h3 class="title is-6 mb-2 ellipsis">{album.title}</h3>
--- web/src/components/artist-card.svelte
+++ web/src/components/artist-card.svelte
@@ -1,26 +1,24 @@
 <script>
     import { musicPlayer } from '../stores.js';
-    
+
     export let artist;
     export let token;
 
-    async function fetchTracks() {
+    async function fetchAndPlayTracks() {
         const response = await fetch(`${import.meta.env.VITE_API_URL}/artists/${artist.id}`, {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
         });
-        let artistData = await response.json();
-        let tracks = artistData['top_tracks'];
-        
-        $musicPlayer.playTracks(tracks, tracks[0]);
+        const completeArtist = await response.json();
+        $musicPlayer.playTracks(completeArtist.top_tracks, completeArtist.top_tracks[0]);
     }
 </script>
 
-<a class="card" href="/artists/{artist.id}">
+<a class="card has-image-play-button" href="/artists/{artist.id}">
     <div class="card-image" style="aspect-ratio: 1;">
         <img src={artist.medium_image} alt="Image of artist {artist.name}" loading="lazy" />
-        <button class="button is-outlined play-button-image" on:click|preventDefault={() => fetchTracks()}>
+        <button class="button image-play-button" on:click|preventDefault={fetchAndPlayTracks}>
             <svg class="icon" viewBox="0 0 24 24">
                 <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
             </svg>
