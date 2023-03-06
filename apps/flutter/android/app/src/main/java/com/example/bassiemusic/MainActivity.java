c2213aeeeb3e8ffe24559c75513333654c47199e
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2023-03-06 21:32:50 +0100

    Change flutter android to java and ios code to objc

---
+++ apps/flutter/android/app/src/main/java/com/example/bassiemusic/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.bassiemusic;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- apps/flutter/android/app/src/main/kotlin/com/example/bassiemusic/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bassiemusic
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
