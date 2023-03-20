f96074574290973a4753022e405ec23c11e27934
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2023-03-20 17:41:10 +0100

    Fix wrong icon colors

--- web/src/routes/playlists/[id]/+page.svelte
+++ web/src/routes/playlists/[id]/+page.svelte
@@ -111,7 +111,7 @@
                 </button>
             {:else}
                 <button class="button is-large" on:click={likePlaylist} title={t('remove_like')}>
-                    <svg class="icon is-colored" viewBox="0 0 24 24">
+                    <svg class="icon" viewBox="0 0 24 24">
                         <path
                             fill="#f14668"
                             d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z"
@@ -122,14 +122,14 @@
 
             {#if data.playlist.user.id == data.authUser.id || data.authUser.role == 'admin'}
                 <button class="button is-large" on:click={() => editModal.open()} title={t('edit')}>
-                    <svg class="icon is-colored" viewBox="0 0 24 24">
+                    <svg class="icon" viewBox="0 0 24 24">
                         <path
                             d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z"
                         />
                     </svg>
                 </button>
                 <button class="button is-large" on:click={() => deleteModal.open()} title={t('delete')}>
-                    <svg class="icon is-colored" viewBox="0 0 24 24">
+                    <svg class="icon" viewBox="0 0 24 24">
                         <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
                     </svg>
                 </button>
